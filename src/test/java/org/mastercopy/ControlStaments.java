35b91a8be9af4bff133023651064e9e927e24b98
gowthamidesingu <gowthamisanth04@gmail.com>
2021-11-30 15:19:30 +0530

    gowthami

--- src/test/java/org/mastercopy/ControlStaments.java
+++ src/test/java/org/mastercopy/ControlStaments.java
@@ -1,22 +1,11 @@
 package org.mastercopy;
 
 public class ControlStaments {
-
+private void emp() {
+	System.out.println();
+}
 	public static void main(String[] args) {
 		
-	int empid= 3000;
-	if (empid==3737) {
-		System.out.println("valid employee id");
-	}
-	else {
-	System.out.println("invalid employee id");	
-	}
-	else {
-		System.out.println("invalid employee id");	
-		}
-	else {
-		System.out.println("invalid employee id");	
-		}
 	}
 }
 		

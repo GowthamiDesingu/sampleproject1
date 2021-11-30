20dc7406d2eb1074946949b9ab1867475f6dae69
gowthamidesingu <gowthamisanth04@gmail.com>
2021-11-30 14:04:56 +0530

    maven project

---
+++ src/test/java/org/mastercopy/ControlStaments.java
@@ -0,0 +1,16 @@
+package org.mastercopy;
+
+public class ControlStaments {
+
+	public static void main(String[] args) {
+		
+	int empid= 3000;
+	if (empid==3737) {
+		System.out.println("valid employee id");
+	}
+	else {
+	System.out.println("invalid employee id");	
+	}
+	}
+}
+		
---
+++ src/test/java/org/mastercopy/Nestedif.java
@@ -0,0 +1,24 @@
+package org.mastercopy;
+
+public class Nestedif {
+
+	public static void main(String[] args) {
+		
+		int age = 25, weight= 65;
+	
+		if (age > 18) {
+			if (weight >= 60) {
+				 System.out.println("Able to blood donated");
+			}
+			else {
+			System.out.println("unble to donated");
+			}
+//		    else {
+//		       System.out.println("..........");
+//		}
+	
+		}	
+		
+	}
+}
+
